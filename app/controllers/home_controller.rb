cef414229f72aebf7352aa7c9a81319b0068074e
dreamlx <dreamlinx@gmail.com>
2013-06-26 17:05:32 +0800

    init haml

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
+# coding: utf-8
 class HomeController < ApplicationController
   def index
+    @weibo_items = []
+    @weibo_items << { content: "热门事件A" }
+    @weibo_items << { content: "热门事件B" }
+    @weibo_items << { content: "热门事件C" }
+    @weibo_items << { content: "热门事件D" }
   end
 end
