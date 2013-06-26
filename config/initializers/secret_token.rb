dd6cc18bcb9c0f43152643ed5ecfc9e054890d03
dreamlx <dreamlinx@gmail.com>
2013-06-26 13:06:10 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skynet::Application.config.secret_token = 'dcf9d1b9003bec918e81c4e645047e70f29e36888041db0a7bcda13bbc3329861afd804d2153d091c726abf7636f159e4c6a375467e9836a101c6a029e81d351'
