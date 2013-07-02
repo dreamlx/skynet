4c44813fac46f812e2b5a4bd2cc4c2a9484fe7a3
dreamlx <dreamlinx@gmail.com>
2013-07-02 20:16:10 +0800

    touch login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,49 @@
 # coding: utf-8
 class HomeController < ApplicationController
   def index
-    @lastestWeiboArticle =[]
-    @lastestWeiboArticle[1] = "最新舆情,最新舆情最新舆情最新舆情最新舆情最新舆情最新舆情最新舆情最新舆情最新舆情"
-    @lastestWeiboArticle[2] = "最新预警,最新预警最新预警最新预警最新预警最新预警最新预警最新预警最新预警最新预警"
+    #through curl login skynet
+    api_url = 'http://imws.voc.com.cn/cgi-bin/imetrics/api'
+    login_api = 'api_login.cgi'
+    act = 'login'
+    username = 'changhe@163.com'
+    password = 'voc2013'
+    id_cp = 30
+    curl = "#{api_url}/#{login_api}?act=#{act}&username=#{username}&password=#{password}&id_cp=#{id_cp}"
     
-    @weibo_items = []
-    @weibo_items << { content: "热门事件A" }
-    @weibo_items << { content: "热门事件B" }
-    @weibo_items << { content: "热门事件C" }
-    @weibo_items << { content: "热门事件D" }
+    http = Curl::Easy.new(curl)
+    http.follow_location = true
+    http.enable_cookies = true
+
+    if http.http_get
+      # puts hash_login: 
+      # {"code"=>"200", "data"=>{"id_cp"=>30, "id_gp"=>183, "VOC_CenterID"=>"432bee3c62e2dcf01ef76a87b59ba49178", "url"=>"http://imws.voc.com.cn/imetrics/30_183.html"}} 
+      hash_login =  ActiveSupport::JSON.decode(http.body_str)
+
+      #get weibo_list
+      weibo_api = 'api_index.cgi'
+      id_gp = hash_login['data']['id_gp']
+      id_cp = hash_login['data']['id_cp']
+
+      act = 'get_wb_list'
+      curl = "#{api_url}/#{weibo_api}?act=#{act}&id_gp=#{id_gp}&id_cp=#{id_cp}"
+      http.url = curl
+      @weibo_list = ActiveSupport::JSON.decode(http.body_str)['arts'] if http.http_get
+
+      #舆情
+      act = 'get_yq_list'
+      curl = "#{api_url}/#{weibo_api}?act=#{act}&id_gp=#{id_gp}&id_cp=#{id_cp}"
+      http.url = curl
+      @weibo_yq = ActiveSupport::JSON.decode(http.body_str)['arts'] if http.http_get
+
+      #预警
+      act = 'get_yj_list'
+      curl = "#{api_url}/#{weibo_api}?act=#{act}&id_gp=#{id_gp}&id_cp=#{id_cp}"
+      http.url = curl
+      @weibo_yj = ActiveSupport::JSON.decode(http.body_str)['arts'] if http.http_get
+
+    else
+        #TODO, try again
+    end
 
     @weiboTable1 =  []
     @weiboTable1<< {content: "新疆好风景A", meiti: "CCTV", created_at: Time.now, url: '#' }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,13 @@ module ApplicationHelper
     #产品-分类-网站名称
     title = "首页 - 天网舆情"
   end
+
+  def get_home_title( lng = "cn" )
+    
+    if lng == 'en'
+      title = "Public Opinion Monitoring System" 
+    else
+      title = "舆情预警分析系统" 
+    end
+  end
 end
