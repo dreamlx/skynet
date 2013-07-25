381f16f8db250735dd3b958ade961b26f3347f25
dreamlx <dreamlinx@gmail.com>
2013-07-25 10:43:43 +0800

    modified:   app/controllers/home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,11 @@ class HomeController < ApplicationController
     if !session[:current_user]
       api_port     = "api_index.cgi"
 
+      #get weibo_list
+      act         = 'get_wb_list'
+      @weibo_list = get_voc(api_port, act)['arts']
+      puts_to_yaml(@weibo_list, "weibo_list")
+
       #舆情
       act         = 'get_yq_list'
       @weibo_yq   = get_voc(api_port, act)['arts']
@@ -96,14 +101,8 @@ class HomeController < ApplicationController
     end
   end
 
-  def weiboList
-    #get weibo_list
-    @weibo_list = get_voc('api_index.cgi', 'get_wb_list')['arts']
-    puts_to_yaml(@weibo_list, "weibo_list")
-    
-    respond_to do |format|
-      format.json { render json: @weibo_list }
-    end
+  def aside
+    render layout: false;
   end
 
   private
