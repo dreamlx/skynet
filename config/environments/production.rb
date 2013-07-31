58104271ef810bbb97308ac9cc5d805a78f71100
dreamlx <dreamlinx@gmail.com>
2013-07-31 10:26:08 +0800

    modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Skynet::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -57,7 +57,11 @@ Skynet::Application.configure do
     colorDefine.css
     frontservices.css
     users.css
+    frontabout.css    
+    frontcases.css
     )
+
+
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -67,7 +71,7 @@ Skynet::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
+config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
